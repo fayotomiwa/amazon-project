7f48b00f0ed77ee0164a8f6fdb1ef1f54727eec2
Techhsiss <techhsiss@gmail.com>
2025-10-21 12:25:14 +0100

    mark cart quantity interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -78,7 +78,12 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
+      let cartQuantity = 0;
 
-      console.log(cart);
+      cart.forEach((item) => {
+        cartQuantity+= item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
   }); 
